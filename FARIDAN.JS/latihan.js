1715c96350ee8937d43d0183b53a5e22f1014129
faridhansign <faridhansign@gmail.com>
2023-06-08 08:40:17 +0700

    haha

---
+++ FARIDAN.JS/latihan.js
@@ -0,0 +1,30 @@
+// var array= []
+// array[0] = "ayam"
+// array[1] = "kancil"
+// array[2] = "buaya"
+// // //array[0] = undefined
+// // for (let index = 0; index < array.length; index++) {
+// //     const element = array[index];
+  
+// //     console.log(array)
+// // }
+
+// // //array.length ()
+// // console.log(array.length)
+// // //array.join("")
+// // console.log(array.join())
+
+// array.push("faridan")
+// array.pop()
+// array.unshift("gita")
+// // array.shift()
+// array.splice(1,1)
+// array.splice(1,0,"gitaku")
+
+// console.log(array)
+
+let nomor = [2,3,4]
+const nomor1 = nomor.map((angka)=>angka * 2)
+console.log(nomor1)
+const nomor2 = nomor.filter((angka)=>angka >=2)
+console.log(nomor2)
